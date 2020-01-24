d83627f174447a97cc9b096f5c678644e5a2b204
Alex Skreen <skreenalex@gmail.com>
2020-01-24 13:54:03 -0800

    created array to push for loop i values to

--- js/scripts.js
+++ js/scripts.js
@@ -4,17 +4,27 @@ function beepBoop(userInput) {
   var result = []
   for (var i = 0; i <= userInput; i++)
   if (i.toString().includes("3")){
-      console.log("I'm sorry, Dave. I'm afraid I can't do that.")
-    }
-    else if (i.toString().includes("2")) {
-      console.log("Boop!")
-    }
-    else if (i.toString().includes("1")) {
-      console.log("Beep!")
+    console.log("I'm sorry, Dave. I'm afraid I can't do that.")
+    result.push[i.toString()];
+    //  return result.push["I'm sorry, Dave. I'm afraid I can't do that."]
+  }
+  else if (i.toString().includes("2")) {
+    console.log("Boop!")
+    result.push[i.toString()];
+    //  return result.push["Boop!"]
+  }
+  else if (i.toString().includes("1")) {
+    console.log("Beep!")
+    result.push[i.toString()];
+      //  return result.push["Beep!"]
     }
     else {
       console.log("Your number is " + i)
+      result.push[i.toString()];
+      // return result.push[i]
     }
+    console.log(result)
+;    // console.log(result);
 };
 
 // Front-end
