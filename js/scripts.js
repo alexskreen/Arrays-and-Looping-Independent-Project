1a0282f281ca560959f62fbd3dbe2f883c571e33
Alex Skreen <skreenalex@gmail.com>
2020-01-24 11:42:03 -0800

    add if and else if statements for beep-boop-error. Won't list out numbers AND go through if else

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,24 @@
-$(document).ready(function(){
-  var results = []
-  function beepBoop(userInput){
-    for(var i = 0; i<=userInput; i++)
-    
-    if(i <= userIgit cnput){
-      console.log("value of i is" + i);
-      }
+var results = []
+function beepBoop(userInput){
+  var valueString = userInput.toString();
+  for(var i = 0; i<userInput; i++)
+  if (valueString.includes("3")){
+    console.log("I'm sorry, Dave. I'm afraid I can't do that.")
   }
+  else if (valueString.includes("2")){
+    console.log("Boop!")
+  }
+  else if (valueString.includes("1")){
+    console.log("Beep!")
+  }
+  else {
+    console.log("Your number is " + i)
+  }
+  console.log(results);
+};
+  // function inputToArr(userInput){
+  //   for(var i = 0; i <= userInput; i++)
+$(document).ready(function(){
   $("#submitButton").click(function (event) {
     var userInput = parseInt($("input#userInput").val());
     event.preventDefault();
