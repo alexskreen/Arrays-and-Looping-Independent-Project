fde6bb8ffb56df61de5e06c451db453866f40500
Alex Skreen <skreenalex@gmail.com>
2020-01-24 10:27:29 -0800

    add back-end logic that counts up to user inputted number

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
-
+  var results = []
+  function beepBoop(userInput){
+    for(var i = 0; i<=userInput; i++)
+    
+    if(i <= userIgit cnput){
+      console.log("value of i is" + i);
+      }
+  }
+  $("#submitButton").click(function (event) {
+    var userInput = parseInt($("input#userInput").val());
+    event.preventDefault();
+    // console.log(userInput)
+    beepBoop(userInput);
+  });
 });
