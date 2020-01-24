8b88fa6dcdb126377795a8718d344d57bd9e6f45
Alex Skreen <skreenalex@gmail.com>
2020-01-24 12:14:23 -0800

    Trying to get if else if statements to work with the counting function

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,32 @@
-var results = []
-function beepBoop(userInput){
+// Back-end
+
+function beepBoop(userInput) {
   var valueString = userInput.toString();
-  for(var i = 0; i<userInput; i++)
-  if (valueString.includes("3")){
-    console.log("I'm sorry, Dave. I'm afraid I can't do that.")
-  }
-  else if (valueString.includes("2")){
-    console.log("Boop!")
-  }
-  else if (valueString.includes("1")){
-    console.log("Beep!")
-  }
-  else {
-    console.log("Your number is " + i)
-  }
-  console.log(results);
+  for (var i = 0; i < userInput; i++)
+    if (valueString.includes("3")) {
+      console.log("I'm sorry, Dave. I'm afraid I can't do that.")
+    }
+    else if (valueString.includes("2")) {
+      console.log("Boop!")
+    }
+    else if (valueString.includes("1")) {
+      console.log("Beep!")
+    }
+    else {
+      console.log("Your number is " + i)
+    }
 };
-  // function inputToArr(userInput){
-  //   for(var i = 0; i <= userInput; i++)
-$(document).ready(function(){
+
+// Front-end
+
+$(document).ready(function () {
   $("#submitButton").click(function (event) {
-    var userInput = parseInt($("input#userInput").val());
     event.preventDefault();
+    // console.log("button works")
+    var userInput = parseInt($("input#userInput").val());
     // console.log(userInput)
+    $("#resultsList").append("<li>" + results + "</li>"); var results = []
     beepBoop(userInput);
+
   });
 });
