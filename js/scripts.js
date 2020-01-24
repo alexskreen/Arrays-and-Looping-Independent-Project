deff58406d65f5f9aafaa7f068eae5f458294757
Alex Skreen <skreenalex@gmail.com>
2020-01-24 15:19:39 -0800

    cleared console logs and alerts. Created function to write array to ul id.

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,46 @@
 // Back-end
 
 function beepBoop(userInput) {
-  var result = []
+  var results = []
   for (var i = 0; i <= userInput; i++)
   if (i.toString().includes("3")){
-    console.log("I'm sorry, Dave. I'm afraid I can't do that.")
-    result.push[i.toString()];
-    //  return result.push["I'm sorry, Dave. I'm afraid I can't do that."]
+    // console.log("I'm sorry, Dave. I'm afraid I can't do that.")
+    results.push["I'm sorry, Dave. I'm afraid I can't do that"];
   }
   else if (i.toString().includes("2")) {
-    console.log("Boop!")
-    result.push[i.toString()];
-    //  return result.push["Boop!"]
-  }
+    // console.log("Boop!")
+    results.push("Boop!");
+    }
   else if (i.toString().includes("1")) {
-    console.log("Beep!")
-    result.push[i.toString()];
-      //  return result.push["Beep!"]
+    // console.log("Beep!")
+    results.push("Beep!");
     }
     else {
-      console.log("Your number is " + i)
-      result.push[i.toString()];
-      // return result.push[i]
+      // console.log("Your number is " + i)
+      results.push(i);
     }
-    console.log(result)
-;    // console.log(result);
+    console.log(results);    
+
+    function writeToPage(results) {
+      $("#resultsList").empty();
+      results.forEach();
+        $("#resultsList").append("<li>" + i +  "</li>");
+
+      };
 };
+
+
 
 // Front-end
 
 $(document).ready(function () {
   $("#submitButton").click(function (event) {
     event.preventDefault();
-    // console.log("button works")
-    var userInput = parseInt($("input#userInput").val());
-    // console.log(userInput)
-    $("#resultsList").append("<li>" + results + "</li>"); var results = []
-    beepBoop(userInput);
 
-  });
-});
+    var userInput = parseInt($("input#userInput").val());    // console.log(userInput)
+    
+    beepBoop(userInput);
+    writeToPage();
+    
+    });
+    });
