80a7383477286c8fe4816775322ca57997c98add
Alex Skreen <skreenalex@gmail.com>
2020-01-24 16:44:49 -0800

    add forEach and empty to display results in ul

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,7 @@ function beepBoop(userInput) {
   for (var i = 0; i <= userInput; i++)
     if (i.toString().includes("3")) {
       // console.log("I'm sorry, Dave. I'm afraid I can't do that.")
-      results.push("glorb glorb");
-      // "I'm sorry, Dave. I'm afraid I can't do that"
+      results.push("I'm sorry, Dave. I'm afraid I can't do that");
     }
     else if (i.toString().includes("2")) {
       // console.log("Boop!")
@@ -22,15 +21,22 @@ function beepBoop(userInput) {
     }
   return results;
 };
+var displayResultList = function(results) {
+  $('ul#resultsList').empty();
+results.forEach(function(item) {
+  $('ul#resultsList').append("<li>" + item + "</li>");
+});
+return;
+}
+
 // Front-end
 
 $(document).ready(function () {
   $("#submitButton").click(function (event) {
     event.preventDefault();
     var userInput = parseInt($("input#userInput").val());
-    var output = beepBoop(userInput);
-    // console.log(output)
-    $("#resultsList").append(output);
-
-  });
+    var outputs = beepBoop(userInput);
+    // $("#resultsList").text(outputs);
+    displayResultList(beepBoop(userInput));
+});
 });
