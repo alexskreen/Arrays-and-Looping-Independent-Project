e19692a0fde9a676836613a372986947129b6481
Alex Skreen <skreenalex@gmail.com>
2020-01-24 16:00:25 -0800

    results array prints to page now

--- js/scripts.js
+++ js/scripts.js
@@ -3,44 +3,34 @@
 function beepBoop(userInput) {
   var results = []
   for (var i = 0; i <= userInput; i++)
-  if (i.toString().includes("3")){
-    // console.log("I'm sorry, Dave. I'm afraid I can't do that.")
-    results.push["I'm sorry, Dave. I'm afraid I can't do that"];
-  }
-  else if (i.toString().includes("2")) {
-    // console.log("Boop!")
-    results.push("Boop!");
+    if (i.toString().includes("3")) {
+      // console.log("I'm sorry, Dave. I'm afraid I can't do that.")
+      results.push("I'm sorry, Dave. I'm afraid I can't do that");
     }
-  else if (i.toString().includes("1")) {
-    // console.log("Beep!")
-    results.push("Beep!");
+    else if (i.toString().includes("2")) {
+      // console.log("Boop!")
+      results.push("Boop!");
+    }
+    else if (i.toString().includes("1")) {
+      // console.log("Beep!")
+      results.push("Beep!");
     }
     else {
       // console.log("Your number is " + i)
       results.push(i);
     }
-    console.log(results);    
-
-    function writeToPage(results) {
-      $("#resultsList").empty();
-      results.forEach();
-        $("#resultsList").append("<li>" + i +  "</li>");
-
-      };
+  return results;
 };
-
-
-
 // Front-end
 
 $(document).ready(function () {
   $("#submitButton").click(function (event) {
     event.preventDefault();
+    var userInput = parseInt($("input#userInput").val());
+    var output = beepBoop(userInput);
+    // console.log(output)
+    output.forEach();
+      $("#resultsList").append(output);
 
-    var userInput = parseInt($("input#userInput").val());    // console.log(userInput)
-    
-    beepBoop(userInput);
-    writeToPage();
-    
-    });
-    });
+  });
+});
